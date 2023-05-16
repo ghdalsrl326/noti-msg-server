80bd0a3c4dc18fead19e67ed72e4c152e9bf472f
minkihong <ghdalsrl326@gmail.com>
2023-05-16 23:48:41 +0900

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const TelegramBot = require("node-telegram-bot-api");
-const token = "5992399200:AAG4RT6sjz4m5ZeK73R2Sv7lr5tUGXx4wRA";
+// const TelegramBot = require("node-telegram-bot-api");
+// const token = "5992399200:AAG4RT6sjz4m5ZeK73R2Sv7lr5tUGXx4wRA";
 
-const bot = new TelegramBot(token, { polling: true });
+// const bot = new TelegramBot(token, { polling: true });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello This is Noti Server");
   console.log("Hello This is Noti Server");
+  res.send("Hello This is Noti Server");
 });
 
-bot.setWebHook;
-
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, "Received your message");
+app.post("/comment", (req, res) => {
+  console.log("Received a webhook from Figma:", req.body);
+  res.status(200).send("Received");
 });
+
+// bot.setWebHook;
+
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
+
+//   bot.sendMessage(chatId, "Received your message");
+// });
 
 // http listen port 생성 서버 실행
 app.listen(8000, () => console.log("Noti - Server is running on port 8000"));
